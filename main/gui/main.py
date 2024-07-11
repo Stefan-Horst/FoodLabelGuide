f414dd2e599c9dd22914e94c06640e8d2ae1cf21
Stefan <stefan-horst@gmx.de>
2024-07-11 03:30:39 +0200

    Add frontend label info streaming

--- main/gui/main.py
+++ main/gui/main.py
@@ -1,19 +1,57 @@
-from flask import Flask, render_template
-import json
-
-with open("resources/label_dict.json", "r", encoding="utf-8") as f:
-    label_dict = json.load(f)
+from flask import Flask, Response, json, render_template
+from time import sleep
+from utils.globals import *
+import utils.util as util
 
 app = Flask(__name__)
+
+label_dict = util.read_label_dict("label_dict.json")
 
 
 @app.route("/")
 def index():
-    detected_labels = ["bio_hexagon", "eco_stars"]
-    accordion_data = [label_dict[key] for key in detected_labels]
+    return render_template("index.html")
 
-    return render_template("index.html", data=accordion_data)
+
+@app.route("/lbl")
+def stream_labels():
+    def gen_labels():
+        model_output = util.get_newest_file_in_dir(DIR_MODEL_RESULTS)
+        while model_output == "": # wait until file exists in directory
+            sleep(0.1)
+            model_output = util.get_newest_file_in_dir(DIR_MODEL_RESULTS)
+        model_labels = util.read_yolo_output(model_output)
+
+        while True:
+            for label in model_labels.keys():
+                img_path, name, description = util.get_label_data(label, label_dict)
+                data = {
+                    "img_path": str(img_path),
+                    "name": name,
+                    "description": description
+                }
+                print(json.dumps(data))
+                yield f"data: {json.dumps(data)}\n\n"
+
+            while True:
+                model_output = util.get_newest_file_in_dir(DIR_MODEL_RESULTS)
+                if model_output == "": # clear labels if directory is cleared
+                    print("empty dir")
+                    yield "data: clear\n\n"
+                    break
+                else:
+                    model_labels_new = util.read_yolo_output(model_output)
+                    if model_labels_new != model_labels: # clear labels then return new ones from outer loop
+                        model_labels = model_labels_new
+                        print("new image")
+                        yield "data: clear\n\n"
+                        break
+                    else:
+                        sleep(0.1)
+    
+    return Response(gen_labels(), mimetype="text/event-stream")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, 
+            threaded=True)
