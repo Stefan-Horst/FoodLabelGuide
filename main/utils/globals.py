a48ec35e0205ce670c400144b8ff7fff537a091e
Stefan Horst <stefan.horst@student.kit.edu>
2024-07-25 02:56:15 +0200

    Update globals, cleanup

--- main/utils/globals.py
+++ main/utils/globals.py
@@ -12,16 +12,13 @@ PROJECT_FOLDER = "main"
 root_path = get_root_path(PROJECT_FOLDER)
 
 
-DIR_GUI = root_path / "gui"
 DIR_RESOURCES = root_path / "gui/resources"
-DIR_BACKEND = root_path / "backend"
 DIR_MODEL = root_path / "backend/model"
 DIR_MODEL_DATA = root_path / "backend/model/data"
-DIR_UTIL = root_path / "util"
 DIR_MODEL_RESULTS = root_path / "backend/model/output" # text files with model prediction data
 
 DIR_IMG_REL = "/static/images/" # food label icons for display on website
 DIR_WEB_RESULT_IMG_REL = "/static/input_images/" # model output images with bounding boxes, web-accessible directory
 
 DIR_MODEL_INPUT = root_path / "backend/input_images" # images taken by camera to be fed into model
-DIR_WEB_RESULT_IMG = root_path / "gui/static/input_images" # model output images with bounding boxes, web-accessible directory
+DIR_WEB_RESULT_IMG = root_path / "gui/image/static/input_images" # model output images with bounding boxes, web-accessible directory
